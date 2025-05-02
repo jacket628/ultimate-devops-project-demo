bb1fbb3d04e3d9adba0f17d4014bb8f5b5ebc8db
jacket628 <275211360@qq.com>
2025-05-02 11:46:09 +0200

    chore: verify github actions 2nd time

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -232,6 +232,7 @@ func (p *productCatalog) Check(ctx context.Context, req *healthpb.HealthCheckReq
 }
 
 func (p *productCatalog) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Health_WatchServer) error {
+	log.Info("Watch called")
 	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
 }
 
@@ -316,12 +317,6 @@ func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error)
 	)
 }
 
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, svcAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
-	)
-}
 
 
 
